7d6b2413aaaa2d3772cb3eb29ab1de64d219d964
viuch3 <viuch3ia@gmail.com>
2026-01-13 18:05:29 -0500

    Se agrega snakeInit

--- snake.py
+++ snake.py
@@ -2,6 +2,11 @@ class Board:
 
     def __init__(self, x, y):
         self.board = [["0" for _ in range(x)] for _ in range(y)]
+        self.snake = [[y // 2, x // 2]]
+
+    def snakeInit(self):
+        if len(self.snake) == 1:
+            self.board[self.snake[0][0]][self.snake[0][1]] = "X"
 
     def __str__(self):
         matrix = []
