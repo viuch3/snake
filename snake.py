c57acc4f223e2e44921815d9e4399fac5f3ff938
viuch3 <viuch3ia@gmail.com>
2026-01-14 12:35:49 -0500

    Se crea una funcion simple de movimiento para la serpiente

--- snake.py
+++ snake.py
@@ -9,8 +9,26 @@ class Board:
         if len(self.snake) == 1:
             self.board[self.snake[0][0]][self.snake[0][1]] = "X"
 
+    def snakeMove(self, move):
+        if move == "h":
+            self.board[self.snake[0][0]][self.snake[0][1]] = "0"
+            self.snake[0][1] -= 1
+            self.snakeInit()
+        elif move == "l":
+            self.board[self.snake[0][0]][self.snake[0][1]] = "0"
+            self.snake[0][1] += 1
+            self.snakeInit()
+        elif move == "k":
+            self.board[self.snake[0][0]][self.snake[0][1]] = "0"
+            self.snake[0][0] -= 1
+            self.snakeInit()
+        elif move == "j":
+            self.board[self.snake[0][0]][self.snake[0][1]] = "0"
+            self.snake[0][0] += 1
+            self.snakeInit()
+
     def __str__(self):
         matrix = []
         for row in self.board:
             matrix.append(" ".join(row))
-        return "\n".join(matrix)
+        return "\n".join(matrix) + "\n"
